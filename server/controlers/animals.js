7edcff3cadcaba7f306d60768e2c221b813e0260
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-23 10:58:27 +0200

    feat: mongoose & created GET, POST, UPDATE, DELETE

---
+++ server/controlers/animals.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const AnimalModel = require('../models/AnimalModel');
+
+const getAnimals =  async (req, res) => {
+
+}
+
+const postAnimal =  async (req, res) => {
+    
+}
+
+const updateAnimal =  async (req, res) => {
+    
+}
+
+const deleteAnimal =  async (req, res) => {
+    
+}
+
+module.exports = {
+
+    getAnimals,
+    postAnimal,
+    updateAnimal,
+    deleteAnimal
+}
---
+++ server/models/AnimalModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    birthDate: {
+        type: Date,
+        required: true
+    },
+},
+{
+    timestamps: true,
+    
+  }
+);
+
+module.exports = mongoose.model('Animal', schema);
