7755334c9260b728a9d7a1b2acf4c9f4cfbb1548
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-23 11:07:01 +0200

    feat: created router

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config({ path:'server/.env'});
-
+const animalRouter = require('./routes/animals')
 
 const  server = express();
 const { SERVER_PORT } = process.env;
 
 //Midlewares
 server.use(cors());
+
+//Add routes
+
+server.use('/animals', animalRouter);
 
 server.get('/', (req, res) => {
     res.send('serveris veikia');
