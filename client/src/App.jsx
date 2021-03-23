16b4dec081bc3621230bd1519fb50eff91eed035
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-23 12:47:59 +0200

    feat: created App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AnimalsManager from './components/AnimalsManager';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App" >
+        <AnimalsManager />
+      </div>
+    );
+  }
+}
+
+export default App;
